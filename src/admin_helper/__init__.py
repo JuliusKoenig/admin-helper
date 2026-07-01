e8e6540f54b9137f98259cf40eb85aaf4dffba22
Julius Koenig <info@bastelquartier.de>
2026-07-01 20:46:13 +0200

    Add metadata attributes to __init__.py for improved package information

--- src/admin_helper/__init__.py
+++ src/admin_helper/__init__.py
@@ -1,4 +1,10 @@
 from importlib.metadata import version
 
 __title__ = "Admin Helper"
+__description__ = "ToDo"
 __version__ = version(__name__)
+__author__ = "Julius Koenig"
+__author_email__ = "info@bastelquartier.de"
+__license__ = "GPL-3.0"
+__license_url__ = "https://www.gnu.org/licenses/gpl-3.0.md"
+__terms_of_service__ = "https://bastelquartier.de/terms-of-service"
