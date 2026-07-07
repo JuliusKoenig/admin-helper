5f7562aa02b82f142651e6c7ba3a2bfc5f49c64a
Julius Koenig <info@bastelquartier.de>
2026-07-07 14:47:17 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
-# remove yarn source list
-rm /etc/apt/sources.list.d/yarn.list
-
-# install supervisor
-echo "Installing supervisor ..."
-apt update && apt install -y supervisor || exit
-mkdir -p "/var/log/supervisor"
-mkdir -p "$ADMIN_HELPER_SUPERVISOR_CONFIG_PATH"
-
-# download binaries
-bash "./download_binaries.sh" || exit
-
-# cd "$ADMIN_HELPER_FILE_BROWSER__PATH" && ./filebrowser config import ./setting.json
-# chmod +x "$ADMIN_HELPER_FILE_BROWSER__PATH/start_filebrowser.sh"
+#
+## remove yarn source list
+#rm /etc/apt/sources.list.d/yarn.list
+#
+## install supervisor
+#echo "Installing supervisor ..."
+#apt update && apt install -y supervisor || exit
+#mkdir -p "/var/log/supervisor"
+#mkdir -p "$ADMIN_HELPER_SUPERVISOR_CONFIG_PATH"
+#
+## download binaries
+#bash "./download_binaries.sh" || exit
+#
+## cd "$ADMIN_HELPER_FILE_BROWSER__PATH" && ./filebrowser config import ./setting.json
+## chmod +x "$ADMIN_HELPER_FILE_BROWSER__PATH/start_filebrowser.sh"
 
 # install uv
 echo "Installing uv ..."
