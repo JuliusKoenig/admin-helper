6b032b4da3c6a7a5f2aee7915ecd50fc4b4b78d6
Julius Koenig <info@bastelquartier.de>
2026-07-08 18:20:34 +0200

    Implement SupervisorLogServer for handling log events via UNIX socket

--- src/admin_helper/supervisor/log_listener.py
+++ src/admin_helper/supervisor/log_listener.py
@@ -1,18 +1,94 @@
+import json
+import os
+import socket
 import sys
+from contextlib import contextmanager
+from dataclasses import field, dataclass
+from pathlib import Path
+
 from supervisor import childutils
+
+os.environ["ADMIN_HELPER_LOGGER__DISABLED"] = "1"  # disable all logging
+from admin_helper.settings import RUN_DIRECTORY
+from admin_helper.types.thread_object import ThreadObject
+
+SOCKET_FILE_PATH = (RUN_DIRECTORY / "supervisord" / f"{Path(__file__).with_suffix("").name}.sock").absolute()
+
+
+@dataclass
+class SupervisorLogServer(ThreadObject):
+    socket_file_path: Path = field(default=SOCKET_FILE_PATH)
+
+    __str_name__: str | None = "SupervisorLogServer"
+
+    def __post_init__(self):
+        if not isinstance(self.socket_file_path, Path):
+            self.socket_file_path = Path(self.socket_file_path)
+        super().__post_init__()
+
+    @contextmanager
+    def socket(self):
+        if self.socket_file_path.exists():
+            self.socket_file_path.unlink()
+        self.socket_file_path.parent.mkdir(parents=True, exist_ok=True)
+        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            server.bind(str(self.socket_file_path))
+            server.listen()
+            yield server
+
+        finally:
+            server.close()
+            try:
+                self.socket_file_path.unlink()
+            except FileNotFoundError:
+                pass
+
+    def run(self):
+        with self.socket() as sock:
+            conn, _ = sock.accept()
+            with conn:
+                file = conn.makefile("r")
+                for line in file:
+                    try:
+                        event = json.loads(line)
+                    except json.JSONDecodeError:
+                        self.logger.warning("Invalid JSON: %r", line)
+                        continue
+                    process_name = event["process"]
+                    channel_name = event["channel"]
+                    message = event["message"]
+                    extra = {
+                        "process_name": process_name,
+                        "channel_name": channel_name,
+                    }
+                    self.logger.info(message, extra=extra)
+
+
+def send(event: dict):
+    try:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+            sock.connect(str(SOCKET_FILE_PATH))
+            event_data = (json.dumps(event) + "\n")
+            sock.sendall(event_data.encode())
+    except Exception as e:
+        print(f"Failed to send event: {e}", file=sys.stderr, flush=True)
 
 
 def main():
-    while True:
-        headers, payload = childutils.listener.wait(sys.stdin, sys.stdout)
-        payload_headers, data = childutils.eventdata(payload + "\n")
-
-        process_name = payload_headers.get("processname", "unknown")
-        channel = payload_headers.get("channel", "unknown")
-
-        print(f"[{process_name}:{channel}] {data.rstrip()}", file=sys.stderr, flush=True)
-
-        childutils.listener.ok(sys.stdout)
+    try:
+        while True:
+            headers, payload = childutils.listener.wait(sys.stdin,
+                                                        sys.stdout)
+            payload_headers, data = childutils.eventdata(payload)
+            send({"process": payload_headers["processname"],
+                  "group": payload_headers["groupname"],
+                  "channel": payload_headers["channel"],
+                  "pid": payload_headers["pid"],
+                  "message": data.rstrip()})
+            childutils.listener.ok(sys.stdout)
+    except KeyboardInterrupt:
+        ...
 
 
 if __name__ == "__main__":
