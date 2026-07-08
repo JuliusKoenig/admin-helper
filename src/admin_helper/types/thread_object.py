e6902ab08c8113715db85cf0beee476af05397d3
Julius Koenig <info@bastelquartier.de>
2026-07-08 18:49:39 +0200

    Enhance ThreadObject to support controlled execution loop with delay

--- src/admin_helper/types/thread_object.py
+++ src/admin_helper/types/thread_object.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+import time
 from abc import abstractmethod
 from dataclasses import dataclass, field
 from typing import Optional
@@ -11,6 +12,8 @@ from admin_helper.types.base_object import BaseObject
 class ThreadObject(BaseObject):
     _lock: threading.Lock = field(default_factory=threading.Lock, init=False)
     _thread: threading.Thread = field(default=None, init=False)
+    loop: bool = field(default=True)
+    loop_delay: int = field(default=1)
 
     __str_name__: str | None = "ThreadObject"
 
@@ -45,17 +48,19 @@ class ThreadObject(BaseObject):
             return super().children
 
     def _run(self):
-        first = True
+        self.logger.info(f"Starting {self} ...")
         try:
             while True:
+                start_time = time.perf_counter()
                 try:
-                    if first:
-                        self.logger.info(f"Starting {self} ...")
-                    else:
-                        self.logger.info(f"Restarting {self} ...")
                     self.run()
                 except Exception as e:
                     self.logger.exception(f"Exception occurred: \n{e}")
+                if not self.loop:
+                    break
+                elapsed_time = time.perf_counter() - start_time
+                if self.loop_delay > elapsed_time:
+                    time.sleep(self.loop_delay - elapsed_time)
         except KeyboardInterrupt:
             self.logger.info(f"Stopping {self} ...")
 
